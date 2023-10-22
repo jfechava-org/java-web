6c9121c575d98dcd1dde1febc86096629d6aac9b
John <johnfre@yahoo.com>
2023-10-22 14:32:15 +0200

    Update create_repo.sh

--- scripts/create_repo.sh
+++ scripts/create_repo.sh
@@ -5,8 +5,12 @@
 # @Descripcion: Create a Github repo using Github API.       #
 # @Fecha: 23 October, 2023.                                  #
 ##############################################################
+#Load variables
+REPO_NAME = $1
+TOKEN = $2
+
 # Create a folder
-mkdir ./${{ env.REPO_NAME }}
+mkdir ./${{ REPO_NAME }}
 
 # Initial configuration for git
 git config --global user.email "support@metro-ito.com"
@@ -16,7 +20,7 @@ git config --global user.name "support"
 curl -L \
 -X POST \
 -H "Accept: application/vnd.github+json" \
--H "Authorization: Bearer ${{ secrets.TOKEN }}" \
+-H "Authorization: Bearer ${{ TOKEN }}" \
 -H "X-GitHub-Api-Version: 2022-11-28" \
 https://api.github.com/orgs/jfechava-org/repos \
 -d '{"name":"${{ env.REPO_NAME }}","description":"New Repo from API","homepage":"https://github.com","private":false,"has_issues":true,"has_projects":true,"has_wiki":true}'

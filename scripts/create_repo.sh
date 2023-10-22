76206afe502a3502e52cbc58de9cb15c75819a83
John <johnfre@yahoo.com>
2023-10-22 15:12:45 +0200

    Update create_repo.sh

--- scripts/create_repo.sh
+++ scripts/create_repo.sh
@@ -23,7 +23,7 @@ git config --global user.name "support"
 curl -L \
 -X POST \
 -H "Accept: application/vnd.github+json" \
--H "Authorization: Bearer "${{ TOKEN }}"" \
+-H "Authorization: Bearer $TOKEN" \
 -H "X-GitHub-Api-Version: 2022-11-28" \
 https://api.github.com/orgs/jfechava-org/repos \
--d '{"name":""${{ REPO_NAME }}"","description":"New Repo from API","homepage":"https://github.com","private":false,"has_issues":true,"has_projects":true,"has_wiki":true}'
+-d '{"name":"$TOKEN","description":"New Repo from API","homepage":"https://github.com","private":false,"has_issues":true,"has_projects":true,"has_wiki":true}'

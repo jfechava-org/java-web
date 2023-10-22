a9ec8bfd148a38ac0d816402252cf4594a681609
John <johnfre@yahoo.com>
2023-10-22 14:43:12 +0200

    Update clone_repo.sh

--- scripts/clone_repo.sh
+++ scripts/clone_repo.sh
@@ -1,12 +1,21 @@
+#!/usr/bin/env bash
 ##############################################################
 # @Author:  John Echavarria - GFT                            #
 # @Version: 1.0                                              #
 # @Descripcion: Clone a repo and add files.                  #
 # @Fecha: 23 October, 2023.                                  #
 ##############################################################
-git clone https://${{ secrets.TOKEN }}@github.com/jfechava-org/${{ env.REPO_NAME }}.git
-echo ${{ env.REPO_NAME }}
-cd ${{ env.REPO_NAME }}
+#Load variables
+REPO_NAME="$1"
+TOKEN="$2" 
+# git clone https://${{ secrets.TOKEN }}@github.com/jfechava-org/${{ env.REPO_NAME }}.git
+# echo ${{ env.REPO_NAME }}
+# cd ${{ env.REPO_NAME }}
+
+git clone https://$TOKEN@github.com/jfechava-org/$REPO_NAME.git
+echo $REPO_NAME
+cd $REPO_NAME
+
 git init
 touch readme.md
 git add .
